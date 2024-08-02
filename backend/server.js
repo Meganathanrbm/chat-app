79b8e8e5d76310a0e7f2875c84eb8bfbfe3f4683
Meganathanrbm <meganathanrbm@gmail.com>
2024-08-02 22:34:15 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,12 @@ import apiRouter from "./routers/index.js";
 import { app, server } from "./socket/socket.js";
 import { fetchUsers, userList } from "./utils/userList.js";
 import User from "./db/models/user.model.js";
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,6 +21,12 @@ app.get("/", (req, res) => {
   res.send("Server is Live⚡");
 });
 app.use("/api", apiRouter);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));  
+});
 
 server.listen(PORT, () => {
   connectToDB().then(async () => {
