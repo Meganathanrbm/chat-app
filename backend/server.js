df1dfc5b8fdcf71d96122686eb698e99a4774ddf
Meganathanrbm <meganathanrbm@gmail.com>
2024-08-03 23:03:32 +0530

    deployment fix

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import connectToDB from "./db/connectToDB.js";
 import cookieParser from "cookie-parser";
 import apiRouter from "./routers/index.js";
 import { app, server } from "./socket/socket.js";
 
-import User from "./db/models/user.model.js";
-import path from "path";
+
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
@@ -22,10 +22,10 @@ app.use(cookieParser());
 // });
 app.use("/api", apiRouter);
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
