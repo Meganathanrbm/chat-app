7a7be8dba4b73cd81ac6d7204848eef86efa173a
Meganathanrbm <meganathanrbm@gmail.com>
2024-08-03 22:31:36 +0530

    deployment vite.json fix

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,10 @@ app.get("/", (req, res) => {
 });
 app.use("/api", apiRouter);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));  
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));  
 });
 
 server.listen(PORT, () => {
